2732af5e284a01cccd6fbb04f932e830f90628ae
Aadhil Ahamed <aadhil2k4@gmail.com>
2024-08-08 23:19:09 +0530

    Image generation

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,28 @@
-let () = print_endline "Hello, World!"
+open Rays
+
+let sample_image () =
+  let image_width = 256 in
+  let image_height = 256 in
+  let render ~row ~col = 
+    let r = float_of_int row /. float_of_int (image_height - 1) in
+    let g = float_of_int col /. float_of_int (image_width - 1) in
+    let b = 0.0 in  
+    let factor = 255.999 in
+    let r = factor *. r |> int_of_float in
+    let g = factor *. g |> int_of_float in
+    let b = factor *. b |> int_of_float in
+    Pixel.create (r, g, b)
+  in
+  Image.create ~height:image_height ~width:image_width ~f:render
+
+let () =
+  let oc = Out_channel.open_text "sample.ppm" in
+  try
+    let fmt = Format.formatter_of_out_channel oc in
+    let img = sample_image () in 
+    Format.fprintf fmt "%a" Image.pp_ppm img;
+    Format.printf "Image generated\n";
+    Out_channel.close oc
+  with ex ->
+    Format.eprintf "%s" (Printexc.to_string ex);
+    Out_channel.close_noerr oc
--- lib/rays.ml
+++ lib/rays.ml
@@ -1,20 +1,21 @@
 open Stdlib.StdLabels
 
-module F = Format (*import Format library*)
+module F = Format
 
 module Pixel = struct
-  type t = int (*t-->int*)
+  type t = int
+
   let create (r, g, b) =
     let r = (r land 255) lsl 16 in
     let g = (g land 255) lsl 8 in
     let b = (b land 255) in
     r lor g lor b
 
-  let r t = (t lsr 16) 
+  let r t = (t lsr 16)
   let g t = (t lsr 8) land 255
   let b t = t land 255
 
-  let zero = create (0,0,0)
+  let zero = create (0, 0, 0)
 
   let pp_ppm fmt t = 
     F.fprintf fmt "%d %d %d" (r t) (g t) (b t)
@@ -25,20 +26,22 @@ module Image = struct
 
   let create ~height ~width ~f =
     let pixels = Array.make_matrix ~dimx:height ~dimy:width Pixel.zero in
-    for row = 0 to height -1 do
+    for row = 0 to height - 1 do
       for col = 0 to width - 1 do
         pixels.(row).(col) <- f ~row ~col
       done
     done;
-    {pixels}
+    { pixels }
 
-  let pp_ppm fmt {pixels} =
+  let pp_ppm fmt { pixels } =
     let nums_rows = Array.length pixels in
     let nums_cols = Array.length pixels.(0) in
-    F.fprintf fmt "P3\n%d %d\n\255\n" nums_cols nums_rows;
-    for row = 0 to nums_rows -1 do 
-      for col = 0 to nums_cols -1 do
-        F.fprintf fmt "%a\n" Pixel.pp_ppm pixels.(row).(col);
-      done
+    F.fprintf fmt "P3\n%d %d\n255\n" nums_cols nums_rows;
+    for row = 0 to nums_rows - 1 do
+      for col = 0 to nums_cols - 1 do
+        Pixel.pp_ppm fmt pixels.(row).(col);
+        F.fprintf fmt " "
+      done;
+      F.fprintf fmt "\n"
     done
 end
